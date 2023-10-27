410623c6107646a7fa4523f10495f0a3a70ed7cd
DESKTOP-E7MAK2G\sriram <e1112909@u.nus.edu>
2023-10-28 02:42:35 +0800

    trying eks cognito in kubernettes

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,5 @@
-ENV_LINK = "https://a44450945f5e04816a392a73fae9bc18-1065398358.ap-southeast-2.elb.amazonaws.com"
+# ENV_LINK = "http://localhost:8080"
+ENV_LINK = "https://a408af3b72fdb4803a341f3dabbeed91-2051859284.ap-southeast-2.elb.amazonaws.com"
 USER_POOL_ID = "ap-southeast-2_OUg9hqyf4"
 CLIENT_ID = "3diqopiha1h5djmvii62kgti38"
 REDIRECT_URI = f"{ENV_LINK}/home"
