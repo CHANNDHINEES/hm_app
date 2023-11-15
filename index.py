9c8744deb82c1a895c68953db575b7017245e406
CHANNDHINEES <e1112909@u.nus.edu>
2023-11-15 21:34:44 +0800

    removing env

--- index.py
+++ index.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
     app.run_server(
         port=port,
         debug=True,
-        host = '0.0.0.0',
+        # host = '127.0.0.1',
         dev_tools_ui=True,
         dev_tools_props_check=False,
     )
